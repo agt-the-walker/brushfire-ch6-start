2b8e00db505913ab5595c5fc552c6fbedf50ebd1
Jerome Zago <jerome.zago@gmail.com>
2017-06-05 00:59:28 +0200

    Preventing duplicate accounts
    Thanks https://forums.manning.com/posts/list/39722.page#p111057

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -70,8 +70,21 @@ module.exports = {
 
             User.create(options).exec((err, createdUser) => {
               if (err) {
+                if (err.invalidAttributes && err.invalidAttributes.email &&
+                    err.invalidAttributes.email[0] &&
+                    err.invalidAttributes.email[0].rule === 'unique') {
+                  return res.send(409, 'Email address is already taken by another user, please try again.');
+                }
+
+                if (err.invalidAttributes && err.invalidAttributes.username &&
+                    err.invalidAttributes.username[0] &&
+                    err.invalidAttributes.username[0].rule === 'unique') {
+                  return res.send(409, 'Username is already taken by another user, please try again.');
+                }
+
                 return res.negotiate(err);
               }
+
               return res.json(createdUser);
             });
           }
--- assets/js/controllers/signupPageController.js
+++ assets/js/controllers/signupPageController.js
@@ -10,11 +10,8 @@ angular.module('brushfire').controller('signupPageController', ['$scope', '$http
     // Set the loading state (i.e. show loading spinner)
     $scope.signupForm.loading = true;
 
-    // // Submit a POST request to Sails. [The signup action has been created.]
-    // $http.post('/user/signup', {
-
-    // Submit a POST request to /user [This is using blueprints.]
-    $http.post('/user', {
+    // Submit a POST request to Sails. [The signup action has been created.]
+    $http.post('/user/signup', {
       email: $scope.signupForm.email,
       username: $scope.signupForm.username.replace(/\s+/g, '-'),
       password: $scope.signupForm.password
@@ -23,7 +20,7 @@ angular.module('brushfire').controller('signupPageController', ['$scope', '$http
 
       // Redirect to the profile page [This is after we have a profile page built]
       // window.location = '#/profile/' + sailsResponse.data.id;
-      
+
       // Redirect to the user blueprint record [This is before we have the profile page built]
       window.location = '/user/' + sailsResponse.data.id;
     })
@@ -32,7 +29,6 @@ angular.module('brushfire').controller('signupPageController', ['$scope', '$http
     // Handle known error type(s).
     if (sailsResponse.status == 409) {
       toastr.error(sailsResponse.data);
-      $scope.signupForm.errorMsg = 'An unexpected error occurred: ' + (sailsResponse.data || sailsResponse.status);
       return;
     }
 
